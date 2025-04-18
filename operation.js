f7dbd472b93fdc330c57c33a07fbe29c12909606
Ahmed Magdy <ahmedmagdee04@gmail.com>
2025-04-18 17:10:50 +0200

    File System First Commit

---
+++ operation.js
@@ -0,0 +1,7 @@
+// ملف operations.js
+export function sum(a, b) {
+    return a + b;
+  }
+  
+//   module.exports = sum; // تصدير الدالة
+  
---
+++ src/index.ts
@@ -0,0 +1,99 @@
+/**
+ * 1- Entry File: the file you start project from
+ *
+ */
+
+import * as http from "http";
+import fs from "fs";
+import path from "path";
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    fs.readFile("index.html", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Internal Serveal Error");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write("<div>");
+        res.write("<h1> Ahmed is better than you </h1>");
+        res.write("</div>");
+        res.end(data);
+      }
+    });
+  } else if (req.url === "/products/new") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(`<html>
+        <head>
+          <title>Add new Product</title>
+        </head>
+
+        <body>
+          <h2>Add new Product</h2>
+          <form action = "/add-product" method ="POST">
+            <label for = "title">Title:</label><br/>
+            <input type = "text" id = "title" name = "title" required/> <br><br>
+            <label for = "description">description:</label><br/>
+            <textarea type = "text" id = "description" name = "description" required></textarea> <br><br>
+            <button type= "submit">Add Product</button>
+      </form>
+        </body>
+      </html>`);
+    res.end();
+  } else if (req.method === "POST" && req.url === "/add-product") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+    req.on("end", () => {
+      const data = new URLSearchParams(body);
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.write(`<div>
+          <h1>Product has been added</h1>
+          <h1>Title: ${data.get("title")}</h1>
+          <h1>description: ${data.get("description")}</h1>
+        </div>`);
+      res.end("<h1>Product has been added</h1>");
+    });
+  } else if (req.url === "/products") {
+    const productFilePath = path.join(__dirname, 'data', 'product.json')
+    
+    fs.access(productFilePath, (err) => {
+      if(err) {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("Product file not found.");
+        return;
+      }
+
+      fs.readFile(productFilePath, 'utf8', (err, data) => {
+        console.log(path.join(__dirname, 'data', 'product.json'));
+        if (err) {
+          res.writeHead(500, { "Content-Type": "text/plain" });
+          res.end("Error reading the products file.");
+          return;
+        }
+        try {
+          const products = JSON.parse(data);
+          res.writeHead(200, { "Content-Type": "application/json" });
+          res.end(JSON.stringify(products))
+        } catch (error) {
+          res.writeHead(500, {'content-type': 'text/plain'})
+          res.end('Error parsing the products file')
+        }
+      })
+
+    })
+
+    
+  } else if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("This is about Page");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("404 - Page not found");
+  }
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log("Server is running on http://localhost:3000");
+});
