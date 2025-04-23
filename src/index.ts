4e80605b193d2177f46b9781d6be19d941e6f5dc
Ahmed Magdy <ahmedmagdee04@gmail.com>
2025-04-23 09:18:55 +0200

    reading each product element

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,39 @@
-import express, { Request, Response } from "express"
+import express, { Request, Response } from "express";
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-    res.send(`<h1>Hello express js</h1>`)
-})
+type Product = {
+  id: number;
+  name: string;
+};
 
-// enpoing for products 
+const dummy_products: Product[] = [
+  { id: 1, name: "Blue T-Shirt" },
+  { id: 2, name: "Blue T-bant" },
+];
+
+app.get("/", (req, res) => {
+  res.send(`<h1>Hello express js</h1>`);
+});
+
 app.get("/products", (req, res) => {
-    res.send([
-        {id: 1, name: "Blue T-Shirt"},
-        {id: 2, name: "Blue T-bant"},
-    ])
-})
-app.get("/products/1", (req: Request, res: Response ) => {
-    res.send( {id: 1, name: "Blue T-Shirt"})
-})
-const PORT : number = 5000
-app.listen(PORT , () => {
-    console.log(`Server running at => http://localhost:${PORT}`)
-})
+  res.send(dummy_products);
+});
+
+app.get("/products/:id", (req: Request, res: Response) => {
+  const productId = +req.params.id;
+  if (isNaN(productId)) {
+    res.status(404).send({ message: "Invalid Product ID" });
+  }
+  const findProduct = dummy_products.find((p) => p.id === productId);
+  if (findProduct) {
+    res.send(findProduct);
+  } else {
+    res.status(404).send({ message: "Invalid Product ID" });
+  }
+});
+
+const PORT: number = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running at => http://localhost:${PORT}`);
+});
