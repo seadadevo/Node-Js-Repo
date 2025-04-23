2d15934fa3ca6d26a15cf9cafd120f960bc78c66
Ahmed Magdy <ahmedmagdee04@gmail.com>
2025-04-23 17:50:44 +0200

    make a post method by using express and thunder client and using middleware for parsing json

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import express, { Request, Response } from "express";
 import { generateFakeProducts } from "../src/utils/fakeData";
 import { IfakeProducts } from "./interfaces";
 const app = express();
-
+app.use(express.json({}));
+app.use(express.urlencoded({ extended: true }));
 const fakeProducts: IfakeProducts[] = generateFakeProducts();
 
 app.get("/", (req, res) => {
@@ -11,7 +12,7 @@ app.get("/", (req, res) => {
 
 app.get("/products", (req, res) => {
   const filterQuery = req.query.filter as string;
-  const allProducts : IfakeProducts[] = fakeProducts
+  const allProducts: IfakeProducts[] = fakeProducts;
 
   let filterProducts: any[] = [];
 
@@ -25,9 +26,9 @@ app.get("/products", (req, res) => {
           filteredProduct[property] = product[property as keyof IfakeProducts];
         }
       });
-      return {id: product.id, ...filteredProduct};
+      return { id: product.id, ...filteredProduct };
     });
-  } else  {
+  } else {
     filterProducts = allProducts;
   }
   res.send(filterProducts);
@@ -46,6 +47,18 @@ app.get("/products/:id", (req: Request, res: Response) => {
   } else {
     res.status(404).send({ message: "Invalid Product ID" });
   }
+});
+
+app.post("/products", (req, res) => {
+  console.log("headers is:", req.headers);
+  console.log("body is:", req.body);
+  const newProduct = req.body;
+  fakeProducts.push({ id: fakeProducts.length + 1, ...newProduct });
+  res.status(201).send({
+    id: fakeProducts.length + 1,
+    title: req.body.title,
+    price: req.body.price,
+  });
 });
 
 const PORT: number = 5000;
